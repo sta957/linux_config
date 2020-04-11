d98638ecb9e1f38cb6caf6b05b48f9105a872a32
Timo S <statim@vmd47025.contaboserver.net>
2020-04-11 10:02:23 +0200

    integrated Vundle into .vimrc

--- README.md
+++ README.md
@@ -3,12 +3,12 @@ My personal linux configuration to stay producitve on every server.
 
 ## What get's modified?
 
-Config | Desc
---- | ---
-.vimrc | Things like line-numbers and syntax highlighting
-.bashrc | Set my own aliase and shortcuts. Also the colorset gets changed
-tools | Install tools like htop, neofetch, speedtestcli, 
-ufw | set ufw-settings to only allow ssh as standard-traffic
+Config | Desc | Mandantory
+--- | --- | ---
+.vimrc | Things like line-numbers and syntax highlighting | x
+.bashrc | Set my own aliase and shortcuts. Also the colorset gets changed | x
+tools | Install tools like htop, neofetch, speedtestcli, git | 
+ufw | set ufw-settings to only allow ssh as standard-traffic |
 
 
 ## How dows it work ?
--- install.sh
+++ install.sh
@@ -1,34 +1,36 @@
 # !/bin/sh
 # Install.sh of personal linux configuration
-# This is for debianbased distros as it uses apt-get
+# This is for debian-based distros as it uses apt-get
+# An internet connection is required!
+#
 # Made by sta957@github in v0.1 on 2020-04-11
 #
 
 
 # update and upgrade ppas
 
-sudo apt update 
-sudo apt -y upgrade
-
+#sudo apt update 
+#sudo apt -y upgrade
 
 # install necessary packages 
 
-sudo apt install -y git
-sudo apt install -y speedtestcli
-sudo apt install -y htop
-sudo apt install -y neofetch
+#sudo apt install -y git
+#sudo apt install -y speedtestcli
+#sudo apt install -y htop
+#sudo apt install -y neofetch
+
 
 # modify .bashrc
 
-$B_FILE = $HOME/.bashrc
+B_FILE="$HOME/.bashrc"
 
 # create .bashrc if not exists
-if test -f "$B_FILE"; then
+if test -f $B_FILE; then
 	touch "$B_FILE"
 	echo "Creating $B_FILE"
 fi	
 
-echo '
+echo ':
 alias ssys="sudo systemctl"
 alias sai="sudo apt install"
 alias saud="sudo apt update"
@@ -36,56 +38,40 @@ alias saug="sudo apt upgrade"
 alias sus="sudo ufw status"
 alias sun="sudo netstat -tulpn"
 alias lsl="ls -l"
-alias fucking="sudo"' | cat - "$B_FILE" > temp && mv temp "$B_FILE"
+alias fucking="sudo"' | cat - $B_FILE > temp && mv temp "$B_FILE"
+
+. "$B_FILE" # enable .bashrc changes
 
 
 # modify .vimrc and install Vundle as packetmanager
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if test -f "/$HOME/.vim/bundle/Vundle.vim"; then
+	git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+fi
 
-$V_FILE= $HOME/.vimrc
+V_FILE="$HOME/.vimrc"
 
-if test -f "$B_FILE"; then
-	touch "$B_FILE"
+if test -f "$V_FILE"; then
+	touch "$V_FILE"
 	echo "Creating $V_FILE"
 fi
 
 echo "
-set nocompatible              \" be iMproved, required
-filetype off                  \" required
-\" set the runtime path to include Vundle and initialize
+set nocompatible
+filetype off
+
 set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
-\" alternatively, pass a path where Vundle should install plugins
-\"call vundle#begin(\'~/some/path/here\')
-\" let Vundle manage Vundle, required
-Plugin \'VundleVim/Vundle.vim\'
-\" The following are examples of different formats supported.
-\" Keep Plugin commands between vundle#begin/end.
-\" plugin on GitHub repo
-Plugin \'tpope/vim-fugitive\'
-\" plugin from http://vim-scripts.org/vim/scripts.html
-\" Plugin \'L9\'
-\" Git plugin not hosted on GitHub
-Plugin \'git://git.wincent.com/command-t.git\'
-\" git repos on your local machine (i.e. when working on your own plugin)
-Plugin \'file:///home/gmarik/path/to/plugin\'
-\" The sparkup vim script is in a subdirectory of this repo called vim.
-\" Pass the path to set the runtimepath properly.
-Plugin \'rstacruz/sparkup\', {\'rtp\': \'vim/\'}
-\" Install L9 and avoid a Naming conflict if you\'ve already installed a
-\" different version somewhere else.
-\" Plugin \'ascenator/L9\', {\'name\': \'newL9\'}
-\" All of your Plugins must be added before the following line
-call vundle#end()            \" required
-filetype plugin indent on    \" required
-\" To ignore plugin indent changes, instead use:
-\"filetype plugin on
-\"" | cat - "$V_FILE" > temp && mv temp "$V_FILE"
+Plugin 'VundleVim/Vundle.vim'
 
-vim +PluginInstall +qall
+\" personal plugins can be installed here
+Plugin 'vim-airline/vim-airline'
+Plugin 'fneu/breezy'
 
-echo "\n\n\n
+call vundle#end()
+filetype plugin indent on
+
+\" personal configuration 
 set number
 set ruler
 set smartcase
@@ -95,8 +81,13 @@ set hlsearch
 set background=dark
 set termguicolors
 
-colorscheme breezy
 let g:airline_theme='breezy'
 let python_highlight_all=1
+
+\"keybindings
+
+nnoremap tn :tabnew<CR>
+nnoremap tc :tabclose<CR>
 " >> "$V_FILE"
 
+vim +PluginInstall +qall
